37468b259e08c6426e2bab504b7ff887c3c1b1bb
CYBAI <cyb.ai.815@gmail.com>
2016-10-21 11:30:17 +0800

    Update mongoid configuration

--- haku/stock_api_server/config/application.rb
+++ haku/stock_api_server/config/application.rb
@@ -14,5 +14,6 @@ module Haku
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    Mongoid.load!("config/mongoid.yml")
   end
 end
