022f64f15e6c90500e60cebb6ddaa053018f7b61
CYBAI <cyb.ai.815@gmail.com>
2016-10-22 22:37:19 +0800

    Default to get turnovers today

--- haku/stock_api_server/app/controllers/turnovers_controller.rb
+++ haku/stock_api_server/app/controllers/turnovers_controller.rb
@@ -1,9 +1,13 @@
 class TurnoversController < ApplicationController
   def index
-    render json: Turnover.all
+    render :json => Turnover.where(
+      :timestamp => DateTime.now.beginning_of_day..DateTime.now.end_of_day
+    )
   end
 
   def show
-    render json: Turnover.all
+    render :json => Turnover.where(
+      :timestamp => DateTime.now.beginning_of_day..DateTime.now.end_of_day
+    )
   end
 end
