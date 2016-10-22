cb32941fe6b47ad7350cd55a420a142d18d96c56
CYBAI <cyb.ai.815@gmail.com>
2016-10-22 21:32:34 +0800

    Covert data to correct type

--- haku/stock_api_server/app/models/Crawler.rb
+++ haku/stock_api_server/app/models/Crawler.rb
@@ -14,15 +14,15 @@ class Crawler
         datas = tr.css('td')
 
         if datas[1].nil? then nil else {
-          :stock_code => datas[1] && datas[1].text,
+          :stock_code => datas[1] && datas[1].text.to_i,
           :stock_name => datas[2] && datas[2].text.strip,
-          :opening_price => datas[3] && datas[3].text.strip,
-          :highest_price => datas[4] && datas[4].text.strip,
-          :lowest_price => datas[5] && datas[5].text.strip,
-          :closing_ytd => datas[6] && datas[6].text.strip,
-          :closing_today => datas[7] && datas[7].text.strip,
-          :trading_volume => datas[8] && datas[8].text.strip,
-          :change => datas[9] && datas[9].text.strip[/[0-9|\.]+/],
+          :opening_price => datas[3] && datas[3].text.strip.to_f,
+          :highest_price => datas[4] && datas[4].text.strip.to_f,
+          :lowest_price => datas[5] && datas[5].text.strip.to_f,
+          :closing_ytd => datas[6] && datas[6].text.strip.to_f,
+          :closing_today => datas[7] && datas[7].text.strip.to_f,
+          :trading_volume => datas[8] && datas[8].text.strip.to_i,
+          :change => datas[9] && datas[9].text.strip[/[0-9|\.]+/].to_f,
           :change_limit => datas[10] && datas[10].text.strip
         } end
       }
