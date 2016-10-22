b403dfcd3477c554763a56585d4a6faa2593ef9c
CYBAI <cyb.ai.815@gmail.com>
2016-10-22 16:05:07 +0800

    Add mongodb schema

--- haku/stock_api_server/app/models/turnover.rb
+++ haku/stock_api_server/app/models/turnover.rb
@@ -1,3 +1,17 @@
 class Turnover
   include Mongoid::Document
+  include Mongoid::Timestamps
+  include Mongoid::Attributes::Dynamic
+
+  field :stock_code, type: Integer
+  field :stock_name, type: String
+  field :opening_price, type: Float
+  field :highest_price, type: Float
+  field :lowest_price, type: Float
+  field :closing_ytd, type: Float
+  field :closing_today, type: Float
+  field :trading_volume, type: Integer
+  field :change, type: Float
+  field :change_limit, type: String
+  field :timestamp, type: Time, default: ->{ Time.now }
 end
