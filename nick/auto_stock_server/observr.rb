05be473a18c4c92b5315a0d82866adc88f920b4c
Chun-MingChen <nick830314@gmail.com>
2016-12-25 19:43:12 +0800

    add option for choosing testing repo

--- nick/auto_stock_server/observr.rb
+++ nick/auto_stock_server/observr.rb
@@ -1,20 +1,22 @@
+REPO_PATH = "./"
+
 def init
   puts 'now start continuous testing :)'
-  run_rspec_with_doc
+  run_rspec_with_doc(REPO_PATH)
 end
 
-def run_rspec_default
+def run_rspec_default(repo = "./")
   puts '============= the following are testing info ======================'
 
   # By default the above will run all _spec.rb files in the spec directory.
-  system "date && bundle exec rspec"
+  system "date && bundle exec rspec " + repo
 end
 
-def run_rspec_with_doc
+def run_rspec_with_doc(repo="./")
   puts '============= the following are testing info ======================'
 
   # By default the above will run all _spec.rb files in the spec directory.
-  system "date && bundle exec rspec -fd"
+  system "date && bundle exec rspec " + repo +" -fd"
 end
 
 # init and run rspec first 
@@ -22,5 +24,5 @@ init
 
 # observe all file in 'spec' folder
 watch('^spec/.*/*_spec\.rb') do |match|
-  run_rspec_with_doc
+  run_rspec_with_doc(REPO_PATH)
 end
