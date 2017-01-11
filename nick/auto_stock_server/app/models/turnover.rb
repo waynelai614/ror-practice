cb84c8a13aa3decf7d7ad84fa98c66dfe935e2c2
Chen, ChunMing <chunming.chen@rakuten.com>
2017-01-11 11:00:24 +0800

    tuning coding style of app/models

--- nick/auto_stock_server/app/models/turnover.rb
+++ nick/auto_stock_server/app/models/turnover.rb
@@ -1,18 +1,19 @@
-class Turnover 
+# Turnover Schema
+class Turnover
   include Mongoid::Document
   include Mongoid::Timestamps
 
   # schema
-  field :stock_number, type: Integer 
+  field :stock_number, type: Integer
   field :stock_name, type: String
-  field :stock_company_hyperlink, type: String 
+  field :stock_company_hyperlink, type: String
   field :stock_opening_price, type: Float
   field :stock_highest_price, type: Float
   field :stock_floor_price, type: Float
-  field :stock_closing_yesterday, type: Float 
+  field :stock_closing_yesterday, type: Float
   field :stock_closing_today, type: Float
   field :stock_volumn, type: Integer
   field :stock_change, type: Float
   field :stock_quote_change, type: String
-  field :timestamps, type: Time, default: -> { Time.now}
+  field :timestamps, type: Time, default: -> { Time.now }
 end
