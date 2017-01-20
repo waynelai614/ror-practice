44c562cc8b96520807584d46eb7445af9d007588
Chen, ChunMing <chunming.chen@rakuten.com>
2017-01-20 21:54:36 +0800

    tuning coding style and refactory the test of controller

--- nick/auto_stock_server/spec/controllers/stock_controller_spec.rb
+++ nick/auto_stock_server/spec/controllers/stock_controller_spec.rb
@@ -1,97 +1,120 @@
-require "rails_helper"
-require "Crawler"
+require 'rails_helper'
+require 'crawler'
 
-RSpec.describe StockController, :type => :controller do
-  describe "GET #index" do
-    context "when responds successfully" do 
-      it {
-        expect(response).to be_success
+RSpec.describe StockController, type: :controller do
+
+  # REST-ful routes
+  describe 'GET #index' do
+    context 'when query data normally' do
+      before(:each) do
+        # stub method 'launch'
+        allow(controller).to receive(:launch)
+          .and_return(build_list(:turnover, Crawler::DATA_COUNT))
         get :index
-        expect(response).to have_http_status(200)
-      }
-      
-    end 
-
-    context "when rendering the index template" do 
-      it {
-        get :index
-        expect(response).to render_template("index")
-      }
-    end 
-
+      end
+      it { expect(response).to have_http_status(200) }
+      it { expect(response).to render_template('index') }
+    end
   end
 
-  describe "POST #create" do
-    before(:each) {
-      Crawler.crawl_data_to_db
-    }
-
-    context "response data type " do 
-      it {
+  describe 'POST #create' do
+    context 'when query data normally' do
+      before(:each) do
+        allow(controller).to receive(:launch)
+          .and_return(build_list(:turnover, Crawler::DATA_COUNT))
         post :create
-        expect(response.content_type).to eq "application/json"
-      }
-    end 
+      end
+      it { expect(response).to have_http_status(200) }
+      it { expect(response.content_type).to eq 'application/json' }
+    end
+  end
 
-    context "without receiving arguments," do 
-      context "today's turnover by default " do 
-        subject {
-          post :create
-          JSON.parse(response.body)
-        }
-        its(:length) {
-          is_expected.to eq Crawler::DATA_COUNT
-        }
-      end 
-      
+  describe '.argument_init' do
+    context "when 'start_date' is reasonable" do
+      subject do
+        controller
+          .argument_init(attributes_for(:requestOpt))
+          .fetch(:start_date)
+      end
+      it { is_expected.to be_instance_of(Time) }
     end
 
-    context "receiving arguments, " do 
-      context "when 'date' is selected" do 
-        subject(:result) {
-          opt = {
-            # given option
-            :start_date => DateTime.yesterday.beginning_of_day,
-            :end_date => DateTime.now.end_of_day
-          }
-          post :create, opt
-          JSON.parse(response.body)
-        }
-        
-        it "length of turnovers should less than DAY * COUNT" do 
-          expect(result.length).to be <= Crawler::DATA_COUNT * 2 
-        end
-      end 
-      context "when 'stock_number' is selected " do 
-        context "and companys exist " do 
-          subject(:result) {
+    context "when 'start_date' is unexpected" do
+      subject do
+        controller
+          .argument_init(attributes_for(:start_date_unexpected))
+          .fetch(:start_date)
+      end
 
-            # get today's turnover and get the stock number of the first and secondcompany
-            stock_number = Crawler.get_daily_data(2).map do |company|
-              company[:stock_number]
-            end 
-            post :create, :stock_number => stock_number
-            JSON.parse(response.body)
-          }
+      it { is_expected.to be_instance_of(Time) }
+      it 'should be today' do
+        expect(Time.now.end_of_day.to_i - subject.to_i).to be < 60 * 60 * 24
+      end
+    end
 
-          it { expect(result).to be_instance_of Array }
-          its(:length) { is_expected.to eq 2 }  
 
-        end 
+    context "when 'end_date' is reasonable" do
+      subject do
+        controller
+          .argument_init(attributes_for(:requestOpt))
+          .fetch(:end_date)
+      end
+      it { is_expected.to be_instance_of(Time) }
+    end
 
-        context "but companys don't exist, result " do 
-          subject(:result) {
-            post :create, :stock_number => [-1]
-            JSON.parse(response.body)
-          }
+    context "when 'end_date' is unexpected" do
+      subject do
+        controller
+          .argument_init(attributes_for(:end_date_unexpected))
+          .fetch(:end_date)
+      end
 
-          # result should be empty array 
-          it { expect(result).to be_instance_of Array }
-          its(:length) { is_expected.to eq 0 }  
-        end 
-        
-      end 
-    end 
+      it { is_expected.to be_instance_of(Time) }
+      it 'should be today' do
+        expect(Time.now.end_of_day.to_i - subject.to_i).to be < 60 * 60 * 24
+      end
+    end
 
-  end 
+    context "when 'stock_number' under expection" do
+    end
+
+    context "when 'stock_number' is unexpected" do
+    end
+  end
+
+  # twos things in launch method
+  # 1. validation
+  # 2. return query result
+  describe '.launch' do
+    context 'without query conditions' do
+    
+    end
+
+    context 'with query condition' do
+      
+      context "when 'start_date' is unexpected" do
+  
+      end
+
+
+    end
+
+    describe 'when query database' do
+      context 'with company' do
+     
+
+
+      end
+
+      context 'without company' do
+      end
+
+
+      context 'fail' do
+
+      end
+
+    end
+
+  end
 end
