f7e852adef542727826e8b0b5be1f268cbfd00de
Wayne Lai <waynelai614@gmail.com>
2017-02-23 17:34:09 +0800

    init rails app

---
+++ wayne/stock_api_server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StockApiServer::Application.config.secret_token = '6fef47693bbfd85f8c1d272bdbaaa40354c54fa19599de173852f376f760d71b3f49a7bf61a7b8416f638f014f7e5d3dc42b5c98162dcce1b0ad05d170637875'
