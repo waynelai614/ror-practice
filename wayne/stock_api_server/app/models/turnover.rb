f87fc200664980db8bdbde80b5df8527f07119a0
Wayne Lai <waynelai614@gmail.com>
2017-03-02 17:54:02 +0800

    Improve readability of large attr_accessor

--- wayne/stock_api_server/app/models/turnover.rb
+++ wayne/stock_api_server/app/models/turnover.rb
@@ -1,3 +1,15 @@
 class Turnover < ActiveRecord::Base
-  attr_accessible :stock_code, :stock_name, :stock_company_uri, :stock_opening_price, :stock_highest_price, :stock_lowest_price, :stock_closing_yesterday, :stock_closing_today, :stock_volume, :stock_change, :stock_quote_change
+  attr_accessible *%i[
+    stock_code
+    stock_name
+    stock_company_uri
+    stock_opening_price
+    stock_highest_price
+    stock_lowest_price
+    stock_closing_yesterday
+    stock_closing_today
+    stock_volume
+    stock_change
+    stock_quote_change
+  ]
 end
