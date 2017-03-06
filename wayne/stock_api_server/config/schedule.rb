4f085351904e3317d82fba40f7af92a0eb29623d
Wayne Lai <waynelai614@gmail.com>
2017-03-06 15:33:42 +0800

    set cron job run every day at 10 am

--- wayne/stock_api_server/config/schedule.rb
+++ wayne/stock_api_server/config/schedule.rb
@@ -3,6 +3,6 @@ set :environment, :development
 # custom job template for docker-compose
 set :job_template, "bash -l -c 'cd :path && :environment_variable=:environment :job'"
 
-every 5.minutes do
+every 1.day, at: '10:00 am' do
   command 'docker-compose run web rails runner Crawler.crawl_data_to_db'
 end
