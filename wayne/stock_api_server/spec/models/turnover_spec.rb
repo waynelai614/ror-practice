1b1717b18c1fc7c1d3403ef81879d0afe212372e
Wayne Lai <waynelai614@gmail.com>
2017-03-08 16:54:23 +0800

    Test factory_girl fake object

--- wayne/stock_api_server/spec/models/turnover_spec.rb
+++ wayne/stock_api_server/spec/models/turnover_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Turnover, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
-  it "is valid with valid attributes" do
+  it "Test factory_girl works" do
     turnover = build(:turnover)
     turnover.stock_code = 1111
     turnover.stock_name = 'test'
