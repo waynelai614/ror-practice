cfc22ef5c2ee87d4ddad681edeb0e777ecf45a4b
Wayne Lai <waynelai614@gmail.com>
2017-03-13 15:59:14 +0800

    Add service to fetch data via http

--- wayne/stock_api_server/frontend/src/app/app.module.js
+++ wayne/stock_api_server/frontend/src/app/app.module.js
@@ -2,10 +2,12 @@ import angular from 'angular';
 
 import componentsModule from './components/components.module';
 import appComponent from './app.component';
+import turnoverService from './components/turnover/turnover.service';
 
 const MODULE_NAME = 'app';
 
 export default angular
   .module(MODULE_NAME, [componentsModule])
+  .service('turnoverService', turnoverService)
   .component(MODULE_NAME, appComponent)
   .name;
--- wayne/stock_api_server/frontend/src/app/components/turnover/turnover.service.js
+++ wayne/stock_api_server/frontend/src/app/components/turnover/turnover.service.js
@@ -1,1 +1,18 @@
-class TurnoversService
+class TurnoverService {
+    constructor($http) {
+      this.$http = $http;
+    }
+    getData() {
+      return this.$http.get('http://0.0.0.0:3000/api/stock.json');
+    }
+    findByStockCode(code) {
+      return this.$http.get('http://0.0.0.0:3000/api/stock.json', {
+        params: {
+          codes: '1314,2303'
+        }
+      });
+    }
+
+}
+
+export default TurnoverService;
