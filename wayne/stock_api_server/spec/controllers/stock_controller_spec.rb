01089872d0bcbde2ec1bd775355eae71ee2c5491
Wayne Lai <waynelai614@gmail.com>
2017-03-17 10:35:02 +0800

    Add GET #date spec, refactor GET #index spec

--- wayne/stock_api_server/spec/controllers/stock_controller_spec.rb
+++ wayne/stock_api_server/spec/controllers/stock_controller_spec.rb
@@ -4,26 +4,45 @@ require 'crawler'
 RSpec.describe StockController, type: :controller do
   let(:data_count) { Crawler::DATA_COUNT }
   let(:filename_test) { 'turnovers.xlsx' }
+  let(:distinct_date) { %w(20170315 20170316) }
   describe 'GET #index' do
     context 'when loads the default data' do
       before(:each) do
+        # without hitting the database
         allow(controller).to receive(:sort_array_of_obj)
           .and_return(build_list(:turnover, data_count))
       end
 
       it 'return JSON data' do
+        # /api/stock.json
         get :index, format: :json
-        turnovers = JSON.parse response.body
         expect(response).to have_http_status(200)
         expect(response.content_type).to eq Mime::JSON.to_s
-        expect(turnovers.length).to eq(data_count)
+        expect((JSON.parse response.body).length).to eq(data_count)
       end
 
       it 'downloads an excel file from respond_to while specifying filename' do
+        # /api/stock.xlsx
         get :index, format: :xlsx
         expect(response.content_type).to eq(Mime::XLSX.to_s)
         expect(response.headers['Content-Disposition']).to include("filename=\"#{filename_test}\"")
         expect(response).to render_template('stock/index')
+      end
+    end
+  end
+
+  describe 'GET #date' do
+    context 'when loads the avaliable date string' do
+      before(:each) do
+        # without hitting the database
+        allow(Turnover).to receive(:find_distinct_date)
+          .and_return(distinct_date)
+      end
+      it 'return JSON data' do
+        get :date
+        expect(response).to have_http_status(200)
+        expect(response.content_type).to eq Mime::JSON.to_s
+        expect((JSON.parse response.body).length).to eq(distinct_date.length)
       end
     end
   end
