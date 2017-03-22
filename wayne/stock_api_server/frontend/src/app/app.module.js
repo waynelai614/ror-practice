a2671b7e57acdd4b31c544a07f1358c206c5e16b
Wayne Lai <waynelai614@gmail.com>
2017-03-22 13:53:45 +0800

    Move turnoverService from components folder to services folder

--- wayne/stock_api_server/frontend/src/app/app.module.js
+++ wayne/stock_api_server/frontend/src/app/app.module.js
@@ -2,7 +2,7 @@ import angular from 'angular';
 
 import componentsModule from './components/components.module';
 import appComponent from './app.component';
-import turnoverService from './components/turnover/turnover.service';
+import turnoverService from './services/turnover/turnover.service';
 
 const MODULE_NAME = 'app';
 
