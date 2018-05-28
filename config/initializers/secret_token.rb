4c7384c27cf2fc9c11dc8ec3f5d31416f2eff01f
yiya <qq7886@hotmail.com>
2018-05-28 10:04:12 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StockApiServer::Application.config.secret_token = '73986edccb30d29794def958fec65af5e122ca173fbba799fd2a95ae0387e5b4bfb8d806814618fe0a97147c352f15a1433131f6ade20a53ba5bfaac3ce20fc6'
