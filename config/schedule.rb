8ae4b31fc8815dcae749df0c024af32aa84142f1
yiya <qq7886@hotmail.com>
2018-06-04 16:43:47 +0800

    cronjob setting

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,18 +1,7 @@
 # Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
-# set :output, '/Users/yiya.chen/Desktop/cron_log.log'
-#
-# every 1.minute do
-  # runner 'Crawler.get_data'
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
+set :output, 'log/cron_log.log'
+every 1.day, at: [
+  '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm'
+  ] do
+  runner 'Crawler.data_to_db'
+end
